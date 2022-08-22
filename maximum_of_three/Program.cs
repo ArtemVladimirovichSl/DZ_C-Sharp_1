d54edfc0cb40dbb3e9822c91b87f28d783242436
Artem <slivnitsin.art@mail.ru>
2022-08-23 02:45:56 +0800

    maximum_of_three

---
+++ maximum_of_three/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2 && num1 > num3)
+{
+    Console.Write("max = ");
+    Console.WriteLine(num1);
+}
+if(num2 > num1 && num2 > num3)
+{
+    Console.Write("max = ");
+    Console.WriteLine(num2);
+}
+if(num3 > num1 && num3 > num2)
+{
+    Console.Write("max = ");
+    Console.WriteLine(num3);
+}
+else
+Console.Write("Одинаковые числа");
